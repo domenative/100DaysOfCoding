54197cb85db7e690765885b73cad6e4a88c47b43
morlang <morlang@platinbaum.de>
2019-01-15 22:18:52 +0100

    initial commit EntityFramework Project

---
+++ 100DaysOfCoding.EntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _100DaysOfCoding.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
